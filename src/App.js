a7d83c04b2223f79bd13086f861bbf7ff8e73b3f
Ravi Sharma <ruravi80@gmail.com>
2021-12-24 19:37:50 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 
 
 
@@ -11,6 +11,7 @@ function App() {
 
     return (
       <div className="App">
+        <Header/>
         <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import './shop-page.styles.scss'
 class ShopPage extends React.Component{
     constructor(props){
         super(props);
